ca3d030308cdd922f4caa8b1681348bbcf88ff85
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-02-21 22:50:33 +0600

    21

--- Self_study/Ashiksir_Class.py
+++ Self_study/Ashiksir_Class.py
@@ -1,26 +1,28 @@
-class Employee:
-    def __init__(self, name, age, salary, joining_date):
-        self.name = name
-        self.age = age
-        self.salary = salary
-        self.joining_date = joining_date
+# class Employee:
+#     def __init__(self, name, age, salary, joining_date):
+#         self.name = name
+#         self.age = age
+#         self.salary = salary
+#         self.joining_date = joining_date
     
-    def incrementSalary(self, value):
-        self.salary += value
+#     def incrementSalary(self, value):
+#         self.salary += value
 
-employee_1 = Employee("Musfique", 21, 150000, "2nd May, 2025")
-employee_2 = Employee("Ahmed", 20, 100000, "9th March, 2026")
-employee_1.incrementSalary(20000)
-print(employee_1.salary)
+# employee_1 = Employee("Musfique", 21, 150000, "2nd May, 2025")
+# employee_2 = Employee("Ahmed", 20, 100000, "9th March, 2026")
+# employee_1.incrementSalary(20000)
+# print(employee_1.salary)
 
 
-class Rectangle:
-    def __init__(self, w,h):
-        self.width = w
-        self.height = h
+# class Rectangle:
+#     def __init__(self, w,h):
+#         self.width = w
+#         self.height = h
     
-    def getArea(self):
-        return self.width * self.height
+#     def getArea(self):
+#         return self.width * self.height
     
-rect = Rectangle(4,5)
-print(rect.getArea())
+# rect = Rectangle(4,5)
+# print(rect.getArea())
+
+
