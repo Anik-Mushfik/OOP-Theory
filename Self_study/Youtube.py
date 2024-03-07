4bbe160bb2de99b52be3ec57ee36889d47e0ef57
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-03-07 13:59:09 +0600

    Update Youtube.py

--- Self_study/Youtube.py
+++ Self_study/Youtube.py
@@ -1,7 +1,17 @@
 class Item:
-    pass
+    def __init__(self, name: str, price: float, quantity = 0):
+        assert price >= 0
+        assert quantity >= 0
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"Item: {self.name} \nPrice: {self.price} \nQuantity: {self.quantity}"
+
+item1 = Item("Musfique", -10000, 13)
+item_2 = Item("Headphone", 300, -12)
+print(item1)
+print(item_2)
+print(type(item1.name))
