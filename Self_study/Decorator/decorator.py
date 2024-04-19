388c52ea007fd04dfc15b0ac38198ee81dec848e
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-04-19 12:41:24 +0600

    Update decorator.py

--- Self_study/Decorator/decorator.py
+++ Self_study/Decorator/decorator.py
@@ -103,9 +103,24 @@ def my_logger(orig_func):
     return wrapper
 
 
+def my_timer(orig_func):
+    import time
 
-@my_logger
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print(f"{orig_func.__name__} ran in: {t2} sec")
+        return result
+    
+    return wrapper
+
+import time
+
+# @my_logger
+@my_timer
 def display_info(name, age):
+    time.sleep(2)
     print(f"display_info ran with arguments ({name}, {age})")
 
 display_info('Abid', 23)
