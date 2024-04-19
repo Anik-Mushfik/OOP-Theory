72f78618de5072480137a24c31555682e0e5eece
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-04-19 12:31:35 +0600

    youtube

--- Self_study/Decorator/decorator.py
+++ Self_study/Decorator/decorator.py
@@ -89,3 +89,23 @@
 
 
 
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename=f"{orig_func.__name__}.log", level=logging.INFO)
+
+    def wrapper(*args, **kwargs):
+        logging.info(
+            f"Ran with args: {args} & kwargs: {kwargs}"
+        )
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+
+@my_logger
+def display_info(name, age):
+    print(f"display_info ran with arguments ({name}, {age})")
+
+display_info('Abid', 23)
