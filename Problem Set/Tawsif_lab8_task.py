89bb91f2f2ae1a40cca79bc38a8725858cfaf771
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-04-19 16:36:06 +0600

    Create Tawsif_lab8_task.py

---
+++ Problem Set/Tawsif_lab8_task.py
@@ -0,0 +1,10 @@
+"""
+Develop a banking system where there is an “Account” class. This class will have an
+attribute “balance” denoting the balance of the account. You can deposit some amount of
+money in the account through a method called deposit(amount). You can also withdraw
+an amount of money through a method called withdraw(amount). But if the user tries to
+withdraw an amount more than their current balance, raise a custom error called
+“NotEnoughBalance”. Inside the withdraw() function, raise this error when appropriate,
+and also deal with this error using try and except blocks.
+"""
+
