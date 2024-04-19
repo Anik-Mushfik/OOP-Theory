0bb312d2aba7432da817a0b6da3065572baf8a02
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-04-19 11:30:00 +0600

    Update decorator.py

--- Self_study/decorator.py
+++ Self_study/decorator.py
@@ -29,16 +29,18 @@
 
 
 def decorator_function(original_fuvtion):
-    def wrapper_function():
+    def wrapper_function(*args, **kwargs):
         print("Wrapper exicuted this before {}".format(original_fuvtion.__name__))
         # return original_fuvtion()
-        original_fuvtion()
+        original_fuvtion(*args, **kwargs)
+        # use (*args) and (*kwargs), so that no error happens even if you use arguments in decorated function.
+        # else this error happens=> TypeError: decorator_function.<locals>.wrapper_function() takes 0 positional arguments but 2 were given
         print(f"Wrapper exicuted this after {original_fuvtion.__name__}")
         
     return wrapper_function
 
 def display():
-    print("Display Message!")
+    print("Display function ran!")
 
 decorated_display = decorator_function(display)
 decorated_display()
@@ -52,4 +54,9 @@ def dissplay():
 dissplay()
 
 
+@decorator_function
+def display_info(name, age):
+    print(f"display_info ran with arguments ({name}, {age})")
+
+display_info('Jhon', 35)
 
