758be7dfa7ac99f6d6667b47cb4b8f399735838e
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-05-16 01:47:34 +0600

    gserg

--- Self_study/Generator/generator.py
+++ Self_study/Generator/generator.py
@@ -1,31 +1,42 @@
-def my_gen():
-    yield 1
-    yield 2
-    yield 4
+# def my_gen():
+#     yield 1
+#     yield 2
+#     yield 4
 
-for i in my_gen():
-    print(i)
+# for i in my_gen():
+#     print(i)
 
+
+# def csv_reader(file_name):
+#     file = open(file_name)
+#     result = file.read().split("\n")
+#     return result
+# csv_gen = csv_reader("D:\Python Study\OOP-Theory\Self_study\Generator\some.txt")
+# row_count = 0
+# for row in csv_gen:
+#     row_count += 1
+# print(f"Row count is {row_count}")
 
 def csv_reader(file_name):
-    file = open(file_name)
-    result = file.read().split("\n")
-    return result
-csv_gen = csv_reader("D:\Python Study\OOP-Theory\Self_study\Generator\some.txt")
-row_count = 0
-for row in csv_gen:
-    row_count += 1
-print(f"Row count is {row_count}")
+    for row in open(file_name, "r"):
+        yield row
+
+hi = csv_reader("D:/Python Study/OOP-Theory/Self_study/Generator/some.txt")
+print(next(hi))
+print(next(hi))
+print(next(hi))
+print(next(hi))
+print(next(hi))
 
 
-def infinite_sequence():
-    num = 0
-    while True:
-        yield num
-        num += 1
-gen = infinite_sequence()
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+# def infinite_sequence():
+#     num = 0
+#     while True:
+#         yield num
+#         num += 1
+# gen = infinite_sequence()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
--- Self_study/exception.py
+++ Self_study/exception.py
@@ -1,28 +1,59 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print('Learn Math First You Eddiot!!')
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print('Learn Math First You Eddiot!!')
 
 
-print("Give me two numbers, and I'll divide them.")
-print("Enter 'q' to quit.")
-while True:
-    first_num = input("\nFirst Number: ")
-    if first_num == "q":
-        break
-    second_num = input("Second Number: ")
-    try:
-        answer = int(first_num) / int(second_num)
-    except ZeroDivisionError:
-        print("You can't divide by 0!")
-    else: print(answer)
+# print("Give me two numbers, and I'll divide them.")
+# print("Enter 'q' to quit.")
+# while True:
+#     first_num = input("\nFirst Number: ")
+#     if first_num == "q":
+#         break
+#     second_num = input("Second Number: ")
+#     try:
+#         answer = int(first_num) / int(second_num)
+#     except ZeroDivisionError:
+#         print("You can't divide by 0!")
+#     else: print(answer)
 
 
 
-def add(a,b):
-    if not isinstance(a,type(1)) or not isinstance(b,type(1)):
-        raise ValueError("Operand not integer type")
-    else:
-        return a+b
-print(add(4,5))
-add(4,"dd")
+# def add(a,b):
+#     if not isinstance(a,type(1)) or not isinstance(b,type(1)):
+#         raise ValueError("Operand not integer type")
+#     else:
+#         return a+b
+# print(add(4,5))
+# add(4,"dd")
+
+
+
+# try:
+#     x = int(input("Enter a number: "))
+#     y = "10 /" + x
+#     print("The result is:", y)
+# except ValueError:
+#     print("You must enter a valid integer.")
+# except ZeroDivisionError:
+#     print("You cannot divide by zero.")
+# except TypeError:
+#     print("Your type is wrong.")
+
+
+
+
+# def add(a,b):
+#     if not isinstance(a,type(1))or not isinstance(b,type(1)):
+#         raise ValueError("Operandnotintegertype")
+#     else:
+#         return a+b
+# try:
+#     print(add(4,"dd"))
+# except ValueError as v:
+#     print(v)
+
+# try:
+#     print(add(4,5))
+# except ValueError as v:
+#     print(v)
